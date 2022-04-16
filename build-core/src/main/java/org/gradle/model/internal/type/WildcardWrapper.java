9bf9c83465b423e826c18e5c46716053de618f41
dingyi222666 <dingyi222666@foxmail.com>
2022-04-16 18:43:41 +0800

    feat: add domain collection and reflect api

--- build-core/src/main/java/org/gradle/model/internal/type/WildcardWrapper.java
+++ build-core/src/main/java/org/gradle/model/internal/type/WildcardWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,19 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.api;
 
-/**
- * A factory for named objects of type {@code T}.
- *
- * @param <T> The type of objects which this factory creates.
- */
-public interface NamedDomainObjectFactory<T> {
-    /**
-     * Creates a new object with the given name.
-     *
-     * @param name The name
-     * @return The object.
-     */
-    T create(String name);
+package org.gradle.model.internal.type;
+
+import javax.annotation.Nullable;
+
+public interface WildcardWrapper extends TypeWrapper {
+    TypeWrapper getUpperBound();
+
+    @Nullable
+    TypeWrapper getLowerBound();
 }
