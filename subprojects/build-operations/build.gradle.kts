b47aaa8d5b4e91e321b60e2bca3475fb56ec9c6d
dingyi222666 <dingyi222666@foxmail.com>
2022-05-22 02:34:13 +0800

    feat: add gradle build-operations module

--- subprojects/build-operations/build.gradle.kts
+++ subprojects/build-operations/build.gradle.kts
@@ -7,8 +7,8 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
+
 dependencies {
     implementation(project(":base-annotations"))
-    implementation("com.google.guava:guava:30.1.1-jre")
-    implementation("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava")
+    implementation ("org.slf4j:slf4j-api:1.7.36")
 }
