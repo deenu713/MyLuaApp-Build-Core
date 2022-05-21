7dc0d077359ed003f3f71a5b2f1243c330d8f82c
dingyi222666 <dingyi222666@foxmail.com>
2022-05-21 23:37:25 +0800

    feat: add gradle base-annotations module

---
+++ subprojects/base-annotations/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
